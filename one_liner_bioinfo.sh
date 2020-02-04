2eda6a59744104513de6c2504e36936a6d373e8a
Alban BESNARD <10758663+besnarda@users.noreply.github.com>
2020-02-04 17:11:58 +0100

    Add credit

--- one_liner_bioinfo.sh
+++ one_liner_bioinfo.sh
@@ -2,3 +2,7 @@
 samtools depth -a snps.bam  | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
 # Same as above but only for one contig
 samtools depth -a snps.bam  | grep contig_name | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
+
+
+# Credit for those who made these one-liners.
+russdurrett @biostars
