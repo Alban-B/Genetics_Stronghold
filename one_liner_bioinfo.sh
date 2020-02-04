eb7a27a3e8a4619ca37fc10ed9d16f0327c36842
Alban BESNARD <10758663+besnarda@users.noreply.github.com>
2020-02-04 17:03:35 +0100

    Add some description

--- one_liner_bioinfo.sh
+++ one_liner_bioinfo.sh
@@ -1,1 +1,4 @@
-samtools depth  snps.bam  | awk '{sum+=$3} END { print "Average = ",sum/NR}'
+# Get the average coverage of a bam file. 
+samtools depth  snps.bam  | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
+# Same as above but only for one contig
+samtools depth  snps.bam  | grep contig_name | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
