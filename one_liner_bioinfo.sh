ed6075f2c2bc1f9da2e1d3714265ca9fcc690124
Alban BESNARD <10758663+besnarda@users.noreply.github.com>
2020-02-04 17:09:15 +0100

    added -a to take into account all positions

--- one_liner_bioinfo.sh
+++ one_liner_bioinfo.sh
@@ -1,4 +1,4 @@
 # Get the average coverage of a bam file. 
-samtools depth  snps.bam  | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
+samtools depth -a snps.bam  | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
 # Same as above but only for one contig
-samtools depth  snps.bam  | grep contig_name | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
+samtools depth -a snps.bam  | grep contig_name | awk '{sum+=$3} END { print "Average = ",sum/NR}' 
